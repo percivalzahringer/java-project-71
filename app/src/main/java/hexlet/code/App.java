c64592f6401fd04606c22404b51f67b87f38d4d4
Kerosene <heaven72nd@gmail.com>
2024-06-21 19:58:20 +0700

    app update

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,37 +1,46 @@
 package hexlet.code;
 
 import picocli.CommandLine;
-import picocli.CommandLine.Option;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
-
 import java.util.concurrent.Callable;
 
-@Command(name = "gendiff",
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 4.7.5",
         description = "Compares two configuration files and shows a difference.")
-public class App implements Callable<Integer> {
+public final class App implements Callable<Integer> {
 
-    static final String HOME = System.getProperty("user.home");
+    private static final int SUCCESS_EXIT_CODE = 0;
+    private static final int ERROR_EXIT_CODE = 1;
 
+    @Parameters(index = "0", description = "path to first file")
+    private String filepath1;
+    @Parameters(index = "1", description = "path to second file")
+    private String filepath2;
     @Option(names = {"-f", "--format"}, defaultValue = "stylish",
-            description = "output format [default: stylish]", paramLabel = "format")
-    String format;
-    @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
-    boolean helpRequested = false;
-    @Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
-    boolean versionInfoRequested = false;
-    @Parameters(index = "0", description = "path to first file",
-            paramLabel = "filepath1") String firstFileName;
-    @Parameters(index = "1", description = "path to second file",
-            paramLabel = "filepath2") String secondFileName;
+            description = "output format [default: ${DEFAULT-VALUE}]")
+    private String format;
 
-    public static void main(String[] args) {
-        System.exit(new CommandLine(new App()).execute(args));
-    }
+    /**
+     * @return returns a string of differences between 2 files
+     */
     @Override
-    public Integer call() throws Exception {
-        String resultOfDiff = Differ.generate(firstFileName, secondFileName, format);
-        System.out.println(resultOfDiff);
-        return 0;
+    public Integer call() {
+
+        try {
+            String formattedDiff =  Differ.generate(filepath1, filepath2, format);
+            System.out.println(formattedDiff);
+
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            return ERROR_EXIT_CODE;
+        }
+
+        return SUCCESS_EXIT_CODE;
+    }
+
+    public static void main(String... args) {
+        System.exit(new CommandLine(new App()).execute(args));
     }
 }
