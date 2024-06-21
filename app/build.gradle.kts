a4a56674f8111698439e32e42d3972fd7f3226c5
Kerosene <heaven72nd@gmail.com>
2024-06-21 15:56:24 +0700

    bg update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,13 @@
-import org.gradle.api.tasks.testing.logging.TestExceptionFormat
-import org.gradle.api.tasks.testing.logging.TestLogEvent
-
 plugins {
-    id("java")
-    jacoco
-    application
-    checkstyle
-    id("io.freefair.lombok") version "8.6"
-}
+    id("application")
+    id("checkstyle")
+    id("jacoco")
+    id("com.adarshr.test-logger") version "3.2.0"
 
-jacoco {
-    toolVersion = "0.8.12"
+    id("se.patrikerdes.use-latest-versions") version "0.2.18"
+    id("com.github.ben-manes.versions") version "0.48.0"
+
+    id("io.freefair.lombok") version "8.6"
 }
 
 application {
@@ -25,39 +22,38 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.2"))
-    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+    implementation("info.picocli:picocli:4.7.5")
 
-    implementation("org.apache.commons:commons-lang3:3.14.0")
-    implementation ("info.picocli:picocli:4.7.5")
+    implementation("org.slf4j:slf4j-api:2.0.9")
+    implementation("ch.qos.logback:logback-classic:1.4.12")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.15.2")
+
+    testImplementation("org.assertj:assertj-core:3.25.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.0")
 
     annotationProcessor("info.picocli:picocli-codegen:4.7.5")
-
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.0")
-
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.17.1")
-    implementation("commons-io:commons-io:2.7")
-
-    implementation("com.google.guava:guava:33.1.0-jre")
-
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.17.1")
 }
 
-tasks.withType<JavaCompile>(){
-    options.compilerArgs.addAll(listOf("-Aproject=${project.group}/${project.name}"))
-}
-
-tasks.test {
-    useJUnitPlatform()
+tasks.withType<Test>() {
     finalizedBy(tasks.jacocoTestReport)
+    useJUnitPlatform()
     testLogging {
-        exceptionFormat = TestExceptionFormat.FULL
-        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
         showStandardStreams = true
     }
 }
 
+tasks.compileJava {
+    options.release = 17
+    options.compilerArgs.add("-Aproject=${project.group}/${project.name}")
+}
+
 tasks.jacocoTestReport {
-    dependsOn(tasks.test)
-    reports { xml.required.set(true) }
+    reports {
+        dependsOn(tasks.test)
+        xml.required.set(true)
+    }
 }
