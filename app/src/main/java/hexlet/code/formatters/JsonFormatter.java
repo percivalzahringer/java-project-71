a41b87ffb50bfa72c4e2387e678fdc102c17e9ff
Kerosene <heaven72nd@gmail.com>
2024-06-26 15:48:40 +0700

    update f

--- app/src/main/java/hexlet/code/formatters/JsonFormatter.java
+++ app/src/main/java/hexlet/code/formatters/JsonFormatter.java
@@ -9,6 +9,6 @@ public class JsonFormatter {
     public static String render(Map<String, Object> differences) throws Exception {
 
         ObjectMapper mapper = new ObjectMapper();
-        return mapper.writeValueAsString(differences);
+        return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(differences);
     }
 }
